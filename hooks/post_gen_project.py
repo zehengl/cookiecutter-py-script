5d56070338365f4cca32f14f615ee4e8911a78de
Zeheng Li <imzehengl@gmail.com>
2023-08-22 09:58:04 -0600

    update template

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,6 +8,10 @@ if not "{{cookiecutter.writing_docs}}" == "yes":
     shutil.rmtree(os.path.join(folder, "docs"))
     os.remove(os.path.join(folder, "mkdocs.yml"))
 
+if not "{{cookiecutter.using_dotenv}}" == "yes":
+    os.remove(os.path.join(folder, ".env"))
+    os.remove(os.path.join(folder, "settings.py"))
+
 message = "initial commit from gh:zehengl/cookiecutter-py-script"
 
 subprocess.call(["git", "init"])
